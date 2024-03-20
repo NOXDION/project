da01a48f7352c618601b54735d0d8d2dc9ddd962
daniel <danisiliente@gmail.com>
2024-03-20 10:56:11 -0500

    Versión Alfa

--- Django/CrudBasico/boards/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from boards.models import Usuarios
-
-class UsuarioForm(forms.ModelForm):
-    class Meta:
-        model=Usuarios
-        fields=['usuNombres', 'usuApellidos']
-        widgets={'usuNombres':forms.TextInput(attrs={'class':'form-control'}),
-                 'usuApellidos':forms.TextInput(attrs={'class':'form-control'}),}
--- Django/CrudBasico/boards/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Usuarios(models.Model):
-    usuNombres = models.CharField(max_length=200)
-    usuApellidos = models.CharField(max_length=200)
-
-    class Meta:
-        db_table = "tbUsuarios"
